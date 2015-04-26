1115fe87209ea08390468ed96a24f08bb87ff956
MazurWiktor <wiktormazur1@gmail.com>
2015-04-26 18:17:42 +0200

    Frontend v.03

---
+++ FRONTEND/js/bootstrap-progressbar.js
@@ -0,0 +1,62 @@
+
+$( function() {
+  var bar = $('#tryProgressBar');
+  var val = null;
+
+  i1 = setInterval(function() {
+    val = parseInt(bar.attr('aria-valuenow'));
+    val += 1;
+    console.log(val);
+    if( val <= 100) {
+      bar.attr('aria-valuenow', val);
+      bar.css('width', val + '%');
+    } else {
+        val = 0;
+        bar.attr('aria-valuenow', val);
+        bar.css('width', val + '%');
+    }
+  }, 100);
+
+});
+
+
+$( function() {
+  var bar = $('#getProgressBar');
+  var val = null;
+
+  i1 = setInterval(function() {
+    val = parseInt(bar.attr('aria-valuenow'));
+    val += 1;
+    console.log(val);
+    if( val <= 100) {
+      bar.attr('aria-valuenow', val);
+      bar.css('width', val + '%');
+    } else {
+        val = 0;
+        bar.attr('aria-valuenow', val);
+        bar.css('width', val + '%');
+    }
+  }, 100);
+
+});
+
+
+$( function() {
+  var bar = $('#checkProgressBar');
+  var val = null;
+
+  i1 = setInterval(function() {
+    val = parseInt(bar.attr('aria-valuenow'));
+    val += 1;
+    console.log(val);
+    if( val <= 100) {
+      bar.attr('aria-valuenow', val);
+      bar.css('width', val + '%');
+    } else {
+        val = 0;
+        bar.attr('aria-valuenow', val);
+        bar.css('width', val + '%');
+    }
+  }, 100);
+
+});
